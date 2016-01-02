7fed4852638425c159f66ee169643e0e7bb70617
Antonin Jacquet <antoninjacquet@gmail.com>
2016-01-02 20:11:42 +0100

    Da capo

--- kernel/include/kernel/idt.h
+++
@@ -1,8 +0,0 @@
-#ifndef _KERNEL_IDT_H
-#define _KERNEL_IDT_H
-#include <defines.h>
-void loadIDTR();//sets up IDT by loading IDTR
-void AddInt(int number, void (*handler)(), dword dpl); //add interrupt
-
-
-#endif
--- libc/math/sqrt.c
+++
@@ -1,6 +0,0 @@
-#include <math.h>
-
-int sqrt(int e)
-{
-    
-}
